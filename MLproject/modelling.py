0b1fe877630878911ed96a4d1d48b18cc830b9c7
farhatfathi <112401818+farhatfathi@users.noreply.github.com>
2025-06-13 13:35:51 +0700

    Update modelling.py hmm

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -23,17 +23,18 @@ X = df.drop("Churn", axis=1)
 y = df["Churn"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 4. Logging & modelling langsung (tanpa start_run)
+# 4. Logging & modelling langsung
 mlflow.set_experiment("churn-prediction")
 
-model = RandomForestClassifier(n_estimators=args.n_estimators, random_state=42)
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=args.n_estimators, random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
-# Logging param, metric, dan model
-mlflow.log_param("n_estimators", args.n_estimators)
-mlflow.log_metric("accuracy", acc)
-mlflow.sklearn.log_model(model, "model")
+    # Logging param, metric, dan model
+    mlflow.log_param("n_estimators", args.n_estimators)
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
 
-print(f"Akurasi model: {acc}")
+    print(f"Akurasi model: {acc}")
